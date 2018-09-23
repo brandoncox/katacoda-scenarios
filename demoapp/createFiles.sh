e8064c5ab27a9abc45e2a93357e0efac11aaf848
Brandon Cox <bcox@redhat.com>
2018-09-22 20:07:12 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/katacoda/scenario-example
+WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
 oc new-project 3scale
-oc create -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=${grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'}
+oc create -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml
+  \ -p WILDCARD_DOMAIN=$WILDCARD
