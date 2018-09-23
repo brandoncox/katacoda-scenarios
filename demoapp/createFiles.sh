748ca83f1456f5462eae7e1eb1461184bbfc01fa
Brandon Cox <bcox@redhat.com>
2018-09-22 20:37:24 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
-IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
-export IP
-export WILDCARD
+export WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
+export IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
 
 oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
 oc new-project 3scale
