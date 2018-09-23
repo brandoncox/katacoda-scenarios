29f98434f11562aa2e008e0ace913bcf70978b7d
Brandon Cox <bcox@redhat.com>
2018-09-22 20:49:26 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -5,4 +5,9 @@ export IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
 
 oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
 oc new-project 3scale
+mkdir /tmp/01 && mkdir /tmp/02 && mkdir /tmp/03 && mkdir /tmp/04
+oc create -f assets/pv1.yaml
+oc create -f assets/pv2.yaml
+oc create -f assets/pv3.yaml
+oc create -f assets/pv4.yaml
 oc new-app -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
