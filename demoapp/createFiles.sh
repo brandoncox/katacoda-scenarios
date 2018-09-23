cc3ffed0ca81fb9953fc43204615c274ecce510b
Brandon Cox <bcox@redhat.com>
2018-09-23 09:12:51 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -5,12 +5,20 @@ export IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
 
 git clone https://github.com/brandoncox/katacoda-scenarios.git
 
-mkdir /tmp/01 && mkdir /tmp/02 && mkdir /tmp/03 && mkdir /tmp/04
-oc create -f katacoda-scenarios/demoapp/assets/pv1.yaml
-oc create -f katacoda-scenarios/demoapp/assets/pv2.yaml
-oc create -f katacoda-scenarios/demoapp/assets/pv3.yaml
-oc create -f katacoda-scenarios/demoapp/assets/pv4.yaml
+docker pull registry.access.redhat.com/3scale-amp22/system:1.7
+docker pull registry.access.redhat.com/3scale-amp22/backend:1.6
+docker pull registry.access.redhat.com/3scale-amp22/apicast-gateway:1.8
+docker pull registry.access.redhat.com/3scale-amp22/wildcard-router:1.6
+docker pull registry.access.redhat.com/rhscl/postgresql-95-rhel7:9.5
+docker pull registry.access.redhat.com/3scale-amp22/zync:1.6
 
-#oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
-#oc new-project 3scale
-#oc new-app -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
+
+mkdir /tmp/01 && mkdir /tmp/02 && mkdir /tmp/03 && mkdir /tmp/04
+oc create -f ./katacoda-scenarios/demoapp/assets/pv1.yaml
+oc create -f ./katacoda-scenarios/demoapp/assets/pv2.yaml
+oc create -f ./katacoda-scenarios/demoapp/assets/pv3.yaml
+oc create -f ./katacoda-scenarios/demoapp/assets/pv4.yaml
+
+oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
+oc new-project 3scale
+oc new-app -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
