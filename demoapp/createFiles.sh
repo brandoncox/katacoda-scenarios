ad9be6635705f14b545f51438f3c3324d5baba4f
Brandon Cox <bcox@redhat.com>
2018-09-22 20:34:51 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -2,6 +2,9 @@
 
 WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
 IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
+export IP
+export WILDCARD
+
 oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
 oc new-project 3scale
 oc new-app -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
