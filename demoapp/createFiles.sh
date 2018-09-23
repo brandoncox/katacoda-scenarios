23b9bed383036fbb4b63b8f23bdc56f8f2c584b2
Brandon Cox <bcox@redhat.com>
2018-09-22 20:18:46 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
+oc login https://master:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
 oc new-project 3scale
-oc create -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
+oc new-app -f https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/2.2.0.GA/amp/amp.yml -p WILDCARD_DOMAIN=$WILDCARD
