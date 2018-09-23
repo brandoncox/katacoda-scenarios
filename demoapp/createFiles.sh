57de29434e7fb639859855b00c4482b9bde5e6c6
Brandon Cox <bcox@redhat.com>
2018-09-23 08:48:00 -0400

    Updated

--- demoapp/createFiles.sh
+++ demoapp/createFiles.sh
@@ -3,11 +3,13 @@
 export WILDCARD=`grep subdomain: /etc/origin/master/master-config.yaml | awk '{print $2}'`
 export IP=`ip addr show eth0 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1`
 
+git clone https://github.com/brandoncox/katacoda-scenarios.git
+
 mkdir /tmp/01 && mkdir /tmp/02 && mkdir /tmp/03 && mkdir /tmp/04
-oc create -f assets/pv1.yaml
-oc create -f assets/pv2.yaml
-oc create -f assets/pv3.yaml
-oc create -f assets/pv4.yaml
+oc create -f katacoda-scenariosdemoapp/assets/pv1.yaml
+oc create -f katacoda-scenariosdemoapp/assets/pv2.yaml
+oc create -f katacoda-scenariosdemoapp/assets/pv3.yaml
+oc create -f katacoda-scenariosdemoapp/assets/pv4.yaml
 
 #oc login https://$IP:8443 --username=demo --password=password --certificate-authority=/etc/origin/master/openshift-master.crt --insecure-skip-tls-verify=true
 #oc new-project 3scale
